12143218dcc2643a66e7210eed03b35627865169
Katy Brown <katybr35@gmail.com>
2022-08-02 11:56:46 -0500

    edited out home page for now

---
+++ typeDefs.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Profile {
+        _id: ID
+        name: String
+        email: String
+        calories: Int
+        friends: [Profile]
+    }
+    type Query {
+        getProfiles: [Profile]
+        getProfile(profileId: ID) Profile
+    }
+    type Mutation {
+        addProfile(name: String!, email: String!, password: String!:) Auth
+        login(email: String!, password: String!): Auth
+        addSkill(profileId: ID!, skill: String!): Profile
+        removeProfile: Profile
+        removeSkill(skill: String!): Profile
+    }
+`;
+
+module.exports = typeDefs;
