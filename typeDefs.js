d1fdb2dfe5cc1213f1ee3175b4ea6f8fce333017
VentSaulCS <96673829+VentSaulCS@users.noreply.github.com>
2022-08-01 21:57:26 -0500

    Add files via upload

--- typeDefs.js
+++ typeDefs.js
@@ -16,6 +16,25 @@ const typeDefs = gql`
         getProfile(profileId: ID) Profile
     }
 
+    type Workout {
+        _id: ID
+        post: String
+        
+    }
+
+    type Query {
+    getWorkout: [Workout]
+    getWorkout(workoutId: ID) Workout
+    }
+
+    type Mutation {
+    addPWorkout(post: String!) Auth
+    removeWorkout: Workout
+
+    }
+
+  
+
     type Mutation {
         addProfile(name: String!, email: String!, password: String!:) Auth
         login(email: String!, password: String!): Auth
